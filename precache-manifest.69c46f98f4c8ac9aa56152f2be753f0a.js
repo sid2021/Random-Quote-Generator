3a50e93de3820904f98ad4dcab14ac1013ea0809
sid2021 <wolanski.jakub@gmail.com>
2020-09-05 14:45:18 +0200

    Updates

---
+++ precache-manifest.69c46f98f4c8ac9aa56152f2be753f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67404eca51c7e2b77f7a0040249885d3",
+    "url": "/Random-Quote-Generator/index.html"
+  },
+  {
+    "revision": "c93e5fd784713eafe9b5",
+    "url": "/Random-Quote-Generator/static/css/main.c126121a.chunk.css"
+  },
+  {
+    "revision": "48faf48efdd1b9a4700b",
+    "url": "/Random-Quote-Generator/static/js/2.1f84fb13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Generator/static/js/2.1f84fb13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93e5fd784713eafe9b5",
+    "url": "/Random-Quote-Generator/static/js/main.166bbf3c.chunk.js"
+  },
+  {
+    "revision": "f18f0d6f1fec1280d173",
+    "url": "/Random-Quote-Generator/static/js/runtime-main.e5a1a36a.js"
+  }
+]);
